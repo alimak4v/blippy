847574cffa764b6b8cfb7bb6902d08bda52ddc8e
lekol <85606378+lekolanv@users.noreply.github.com>
2026-04-25 16:49:48 +0300

    Add files via upload
    Matvei Verbitskii's Iter0. async database, modified main.py (async) and manager.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import sys
+import asyncio
 from manager import BankNode
 from models import State
 
-def main():
+async def main():
     node = BankNode()
 
     try:
-        if not node.startup():
+        if not await node.startup():
             name = input("Введите ник: ")
             for s in name:
                 if (s < 'a' or s > 'z') and (s < "A" or s > "Z"):
                     raise Exception("В нике могут быть только строчнве и заглавные буквы")
             node.state = State(name=name, address="node-"+name, balance=100.0)
-            node.save()
+            await node.save()
     except Exception as e:
         print(e)
         sys.exit()
@@ -26,17 +27,27 @@ def main():
             target = input("Адрес получателя: ")
             try:
                 amt = float(input("Сумма: "))
-                if node.make_tx(target, amt):
+                if await node.make_tx(target, amt):
                     print("Транзакций осуществлена (Борис, реализуйте это)")
                 else:
                     print("Ошибка: недостаточно средств.")
             except:
                 print("Ошибка ввода.")
+
         elif choice == "2":
+            if not node.state.history:
+                print("История пуста.")
             for t in node.state.history:
                 print(f"Перевод: {t.amount} -> {t.to}")
+
         elif choice == "3":
+            await node.save()
+            print("Данные сохранены. До свидания!")
             break
 
+
 if __name__ == "__main__":
-    main()
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
